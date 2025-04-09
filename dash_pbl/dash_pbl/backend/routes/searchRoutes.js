e4efb3e79d1158d6980d9a8ee2c34c44c74f2ddf
Riddhi Shintre <pratikshashintre@gmail.com>
2025-04-09 15:36:18 +0530

    Removed dash_pbl

--- dash_pbl/dash_pbl/backend/routes/searchRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const  { searchAlumni }  = require("../controllers/searchAlumni");
-const { getAlumniDetails } =require("../controllers/AlumniDetails");
-
-router.get('/search-alumni', searchAlumni);
-router.get('/alumni-details/:id' ,getAlumniDetails);
-
-module.exports = router;
